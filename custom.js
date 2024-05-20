eae8c3896dfbd07036075178cafca2240a901465
2sonam1 <sonam.rani.min20@itbhu.ac.in>
2024-05-20 10:39:41 +0530

    changes on navbar

--- custom.js
+++ custom.js
@@ -1,45 +1,30 @@
 $(document).ready(function () {
-    var open = 0;
-    var openSearchBar = 0;
-    var openDropdown = 0;
-    $(".dropdown").hide();
-    $(".cross").hide();
-    $(".search-bar").hide();
-
     $(".search button").click(function () {
-
-        if (openSearchBar == 0) {
-            $(".search-bar").css({"display":"flex"})
-            $(".search-bar").show();
-            $(".search button").css({ "border-top-left-radius": "0", "border-bottom-left-radius": "0", "border-left-style": "none" })
+        var searchBar = $(".search-bar");
+        searchBar.toggle();
+        if (searchBar.css('display') !== 'none') {
+            $(".search button").addClass("search-bar-active");
+        } else {
+            $(".search button").removeClass("search-bar-active");
         }
-        else {
-            $(".search-bar").hide();
-            $(".search button").css({ "border-top-left-radius": "20px", "border-bottom-left-radius": "20px", "border-left-style": "solid" })
-        }
-        openSearchBar = !openSearchBar
     });
-
-    $(".bar button").click(function () {
+    $(".bar button").click(function (){
+        $(".listings").show();
         $(".bar").hide();
-        $(".cross").show();
-        $(".dropdown").show();
-        openDropdown = 1;
-    });
-
+        $(".cross").removeClass("menu-inactive");
+    })
     $(".cross button").click(function () {
+        $(".listings").hide();
+        $(".cross").addClass("menu-inactive");
         $(".bar").show();
-        $(".cross").hide();
-        $(".dropdown").hide();
-        openDropdown = 0;
-    });
+    })
     $(".Motoring").click(function () {
         if (open == 0) {
-            $(".Motoring-links").css({"opacity":"1","visibility":"visible"})
+            $(".Motoring-links").css({"opacity":"1","visibility":"visible","height":"auto"})
             $(".Motoring").css({ "text-decoration": "underline", "color": "rgba(17, 134, 87, 1)" });
         }
         else {
-            $(".Motoring-links").css({ "opacity": "0", "visibility": "hidden" })
+            $(".Motoring-links").css({ "opacity": "0", "visibility": "hidden", "height": "0" })
             $(".Motoring").css({ "text-decoration": "none", "color": "rgba(60, 60, 60, 1)" });
         }
         open = !open
